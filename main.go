0e20692e87cb91e2f17276f3288e07cd2a3024bd
kara9renai <80733629+kara9renai@users.noreply.github.com>
2022-02-16 18:32:10 +0900

    apiにjwt検証Middlewareを追加

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net/http"
 
+	"gihub.com/kara9renai/golang-jwt-api/auth"
 	"github.com/gorilla/mux"
 )
 
@@ -23,11 +24,22 @@ var public = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(post)
 })
 
+var private = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	post := &post{
+		Title: "VGolangとGoogle Cloud Vision APIで画像から文字認識するCLIを速攻でつくる",
+		Tag:   "Go",
+		URL:   "https://qiita.com/po3rin/items/bf439424e38757c1e69b",
+	}
+	json.NewEncoder(w).Encode(post)
+})
+
 func main() {
 	const port = ":8080"
 
 	r := mux.NewRouter()
 	r.Handle("/public", public)
+	r.Handle("/private", auth.JwtMiddleware.Handler(private))
+	r.Handle("/auth", auth.GetTokenHandler)
 
 	if err := http.ListenAndServe(port, r); err != nil {
 		log.Fatal("ListenAndServe:", err)
